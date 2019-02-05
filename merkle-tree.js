90929a901b5a648209aa09e04c95d318c0579596
Rati Montreewat <ratixoxo@gmail.com>
2019-02-05 18:55:19 +0700

    code: Multiple Layers for base and even leaf tree

--- merkle-tree.js
+++ merkle-tree.js
@@ -1,13 +1,35 @@
 class MerkleTree {
-    constructor(leaves, concatHashes){
+
+    constructor(leaves, concatHashes) {
+        // 2 parameters
+        //- An array of leaf nodes for the Merkle Tree.
+        //-A concat function which can be used to concatenate two hashes together to form a new hash.
         this.leaves = leaves;
         this.concatHashes = concatHashes;
     }
-  
-    getRoot(){
-        return this.concatHashes(this.leaves[0], this.leaves[1]);
+
+    getRoot() {
+        let leaves = this.leaves;
+        while (leaves.length > 1) {
+            leaves = this.getNextLevel(leaves);
+        }
+        return leaves[0];
     }
-  
-  }
-  
-  module.exports = MerkleTree;
+
+    getNextLevel(leaves) {
+        let newLevel = [];
+        // Add even length items
+        let evenLength = (leaves.length % 2 == 0) ? leaves.length : leaves.length - 1;
+
+        for (let i = 0; i < evenLength; i += 2) {
+            newLevel.push(this.concatHashes(leaves[i], leaves[i + 1]));
+        }
+
+        return newLevel;
+    }
+
+}
+
+
+module.exports = MerkleTree;
+
